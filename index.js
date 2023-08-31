09f2dce4ab11ff1e1e47b8b40c9837381d700bf2
Isaac Song <isaacsongs10@gmail.com>
2023-08-30 21:25:33 -0700

    DOM Modifying elements Lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Test 1
+document.getElementById("main").remove();
+
+// Test 2
+const newHeader = document.createElement("h1");
+
+// Test 3
+newHeader.id = "victory";
+
+// Test 4
+newHeader.textContent = "is the champion";
